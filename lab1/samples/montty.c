84c0d4fb5f1289ddeeff60c32be82d806c664551
yusong-shen <yusongtju@gmail.com>
2016-02-03 19:24:48 -0600

    Make some no-ops implementations in montty.c

--- lab1/samples/montty.c
+++ lab1/samples/montty.c
@@ -1,5 +1,36 @@
 // montty.c
 
+#include <stdio.h>
+#include <thread.h>
+
+
+// Interrupt Handlers
+void ReceiveInterrupt(int term);
+
+void TransmitInterrupt(int term);
+
+// Device Driver API for User Threads
+int WriteTerminal(int term, char *buf, int buflen);
+
+int ReadTerminal(int term, char *buf, int buflen);
+
+
+int InitTerminal(int term);
+
+
+int TerminalDriverStatistics(struct termstat *stats);
+
+
+int InitTerminalDriver();
 
 
 
+// Implementation
+int InitTerminal(int term){
+	return 0;
+}
+
+
+int InitTerminalDriver(){
+	return 0;
+}
